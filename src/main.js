f505f5acbe5cd3a2cc2a676289ef5cc2e781f877
Sean King <seanianking@gmail.com>
2021-09-15 15:31:44 -0600

    router and custom element added

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+// import store from './store/index'
+import vueCustomElement from 'vue-custom-element'
 
-Vue.config.productionTip = false
+Vue.use(vueCustomElement)
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+// App.store = store
+App.router = router
+Vue.customElement('vue-widget', App)
